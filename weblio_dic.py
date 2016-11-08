362cf5f0cf027775980039ee72d224ed3a2afbf2
114zax514 <xxzaxisxx0901@gmail.com>
2016-11-08 15:39:10 +0900

    second

--- weblio_dic.py
+++ weblio_dic.py
@@ -1,12 +1,16 @@
 #coding:utf-8
 import urllib2
+import sys
+i = True 
+while i: 
+    print('調べたい単語(exitで終了)')
+    word = raw_input('>> ')
+    if(word == 'exit'):
+        sys.exit()
+    fp = urllib2.urlopen('http://ejje.weblio.jp/content/'+word)
+    html = fp.read()
+    fp.close()
 
-print('調べたい単語')
-word = raw_input('>> ')
-fp = urllib2.urlopen('http://ejje.weblio.jp/content/'+word)
-html = fp.read()
-fp.close()
-
-index = html.find('主な意味')
-index2 = html.find('</td></tr></tbody></table></div>')
-print(html[index+51:index2])
+    index = html.find('主な意味')
+    index2 = html.find('</td></tr></tbody></table></div>')
+    print(html[index+51:index2])
